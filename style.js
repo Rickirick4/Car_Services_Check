df11b9270431f84f07a2c6dff92cbd08a34334d2
Rickirick4 <71150839+Rickirick4@users.noreply.github.com>
2021-03-16 21:48:22 +0300

    car_check

---
+++ style.js
@@ -0,0 +1,184 @@
+var Carinfo =[
+    {
+        id : "bmw_116d_1234",
+        modal : "116d",
+        year : 2004,
+        color : "White",
+        servicesCheck: [
+            {
+                id : "bmw_116d_1234_1",
+                date : "20.05.2017",
+                km : 110000,
+                totalPrice : 1000,
+                detail : [
+                    {
+                        id : "bmw_116d_1234_1_1",
+                        description : "price of oil change",
+                        price : 650
+                    },
+
+                    {
+                        id : "bmw_116d_1234_1_2",
+                        description : "price of headlight change",
+                        price : 150
+                    },
+
+                    {
+                        id : "bmw_116d_1234_1_3",
+                        description : "price of filter change",
+                        price : 200
+                    },
+
+                ]
+            },
+
+            {
+                id : "bmw_116d_1234_2",
+                date : "20.05.2018",
+                km : 132000,
+                totalPrice : 900,
+                detail : [
+                    {
+                        id : "bmw_116d_1234_2_1",
+                        description : "price of brake change",
+                        price : 300
+                    },
+
+                    {
+                        id : "bmw_116d_1234_2_2",
+                        description : "price of wheels change",
+                        price : 300
+                    },
+
+                    {
+                        id : "bmw_116d_1234_2_3",
+                        description : "price of bodykit change",
+                        price : 300
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        id : "ford_raptor",
+        modal : "raptor",
+        year : 2016,
+        color : "White",
+        servicesCheck: [
+            {
+                id : "ford_raptor_1",
+                date : "20.05.2017",
+                km : 54000,
+                totalPrice : 1000,
+                detail : [
+                    {
+                        id : "ford_raptor_1_1",
+                        description : "price of oil change",
+                        price : 650
+                    },
+
+                    {
+                        id : "ford_raptor_1_2",
+                        description : "price of headlight change",
+                        price : 150
+                    },
+
+                    {
+                        id : "ford_raptor_1_3",
+                        description : "price of filter change",
+                        price : 200
+                    },
+
+                ]
+            },
+
+            {
+                id : "ford_raptor",
+                date : "20.05.2018",
+                km : 60000,
+                totalPrice : 900,
+                detail : [
+                    {
+                        id : "ford_raptor_2_1",
+                        description : "price of brake change",
+                        price : 300
+                    },
+
+                    {
+                        id : "ford_raptor_2_2",
+                        description : "price of wheels change",
+                        price : 300
+                    },
+
+                    {
+                        id : "ford_raptor_2_3",
+                        description : "price of bodykit change",
+                        price : 300
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        id : "mercedes_amg",
+        modal : "amg",
+        year : 2018,
+        color : "White",
+        servicesCheck: [
+            {
+                id : "mercedes_amg_1",
+                date : "20.05.2019",
+                km : 33000,
+                totalPrice : 1000,
+                detail : [
+                    {
+                        id : "mercedes_amg_1_1",
+                        description : "price of oil change",
+                        price : 650
+                    },
+
+                    {
+                        id : "mercedes_amg_1_2",
+                        description : "price of headlight change",
+                        price : 150
+                    },
+
+                    {
+                        id : "mercedes_amg_1_3",
+                        description : "price of filter change",
+                        price : 200
+                    },
+
+                ]
+            },
+
+            {
+                id : "mercedes_amg",
+                date : "20.05.2020",
+                km : 40000,
+                totalPrice : 900,
+                detail : [
+                    {
+                        id : "mercedes_amg_2_1",
+                        description : "price of brake change",
+                        price : 300
+                    },
+
+                    {
+                        id : "mercedes_amg_2_2",
+                        description : "price of wheels change",
+                        price : 300
+                    },
+
+                    {
+                        id : "mercedes_amg_2_3",
+                        description : "price of bodykit change",
+                        price : 300
+                    }
+                ]
+            }
+        ]
+    }
+]
